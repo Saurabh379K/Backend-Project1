a042d2eb01c5697b31f18146a2e90a834678f360
Saurabh379K <saurabhsemwal2003@gmail.com>
2025-02-19 19:06:23 +0530

    add comment, like, and playlist models; update user logout to unset refresh token

---
+++ src/models/like.model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+    {
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+        },
+        comment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+        likedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Like = mongoose.model("Like", likeSchema)
